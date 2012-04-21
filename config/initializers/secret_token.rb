b8eb7d13959c41765f98231673eca85f6a40c680
Ken Decanio <ken-decanio@41monkeys.com>
2012-04-20 21:51:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorbell::Application.config.secret_token = '2d0943244c469bc1734bb4c7be6ba87d9c3e2787062aeb0977ff87db6fd17c128f1c5f0eecea2a00044273bf0e6b3429ec061d897a9da6fee8a6910d7c979143'
