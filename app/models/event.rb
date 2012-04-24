cd08ebf8d4e767ab60e5eef9dec575a7e7d01d70
Ken Decanio <ken-decanio@41monkeys.com>
2012-04-23 23:08:45 -0700

    events working

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,39 +2,50 @@ class Event < ActiveRecord::Base
   attr_accessible :duration, :frequency, :name, :reserves, :start
   serialize :reserves, Array
   
+  # def initialize (new_name)
+  #   name = new_name
+  #   Rails.logger.debug('************************** event initialize ' + self.inspect)
+  #   
+  # end
+  # 
 
-  def self.sms    
-    
-    Rails.logger.debug('************************** event SMS ' + rest.to_s)
-    
+  def self.sms (params, body)    
     
       phone = params[:From]
-      Rails.logger.debug('************************** command ' + command.to_s)
 
-      case rest[0]
+      case body[1]
         when "new"
-          if event = Event.find_by_name(rest[1]) 
-            return render :text => '<Response><Sms>this event already exists</Sms></Response>', :content_type => "text/xml"
+
+          if event = Event.find_by_name(body[2]) 
+            Rails.logger.debug('************************** event already exists ' + body.to_s)
+            return 'this event already exists'
           end
+          Rails.logger.debug('************************** event need to make new ' + body[2])
           
-          event = Event.new(name => rest[1])
-          render :text => '<Response><Sms>event created</Sms></Response>', :content_type => "text/xml"
+          event = Event.new()
+          event.name = body[2] 
+          event.reserves = []
+          Rails.logger.debug('************************** event looks like this ' + event.inspect)#  + "   the name is " + event.name)
+          
+          event.save!
+          return  'event created'
 
         when "add"
           # event add event_name 
-          event = Event.find_by_name(rest[1]) 
+          event = Event.find_by_name(body[2]) 
           if event 
-            event.reserve << phone
-            render :text => "<Response><Sms>You have been added to the reserves for #{event.name} </Sms></Response>", :content_type => "text/xml"
+            event.reserves << phone
+            event.save
+            return "You have been added to the reserves for #{event.name} "
           else
-            render :text => '<Response><Sms>this event does not exists</Sms></Response>', :content_type => "text/xml"
+            return 'this event does not exists'
           end
           
 
-        else;
-          render :text => "<Response><Sms>SAY WHAT?  Send 'door' to get in/Sms></Response>", :content_type => 'text/xml'
+        else
+          return "SAY WHAT?  Send 'door' to get in"
       end
-    end
+
     
   end
 
