e269e3c111bca12c2b3430a1020cf4e37444f8fa
Ken Decanio <github.com@justthisguy.org>
2012-06-06 16:40:02 -0700

    removed test

--- test/unit/doorman_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DoormanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/doormen_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DoormenHelperTest < ActionView::TestCase
-end
